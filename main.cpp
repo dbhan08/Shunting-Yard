41837ad3a392385e225fceeb15bb9faec2db97a2
dbhan08 <38201130+dbhan08@users.noreply.github.com>
2020-02-13 20:10:00 -0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -5,36 +5,55 @@
 
 using namespace std;
 
-
+bool isEmpty(node* head) {
+    
+    if(head == NULL) {
+        
+        return true;
+        
+    }
+    return false;
+    
+}
 
 
 void enqueue(node* &head,node* current, node* insert) {
+    
 if(head == NULL) {
 
 head = insert;
+   
 
 return;
 }
 
-if(head ->getNext() != NULL) {
+if(current ->getNext() != NULL) {
+
 enqueue(head,current->getNext(), insert);
 
 } else {
+
 current->setNext(insert);
-current->getNext()->setNext(NULL);
+  
+
+insert->setNext(NULL);
 
 }
 }
 
-node* dequeue(node* head) {
+node* dequeue(node* &head, node* current) {
 if(head == NULL) {
 return NULL;
 
+} else if(head->getNext() == NULL) {
+    head = NULL;
+    return NULL;
+        
+        
 } else {
-node* temp = new node(head->getValue());
-temp = head;
-head= head->getNext();
-return temp;
+
+head= current->getNext();
+return current;
 
 }
 
@@ -43,15 +62,15 @@ return temp;
 }
 
 
-void push(node* head, node* insert) {
+void push(node* &head,node* current, node* insert) {
 if(head == NULL) {
 head = insert;
 
 } else {
-if(head->getNext() != NULL) {
-push(head->getNext(), insert);
+if(current->getNext() != NULL) {
+push(head,current->getNext(), insert);
 } else {
-head->setNext(insert);
+current->setNext(insert);
 insert->setNext(NULL);
 
 }
@@ -60,17 +79,22 @@ insert->setNext(NULL);
 
 }
 
-node*  pop(node* head) {
+node*  pop(node* &head,node*current) {
 if(head == NULL) {
 return NULL; 
+} else if(current->getNext() == NULL) {
+    head = NULL;
+    return NULL;
+        
+        
 } else {
-if(head->getNext()->getNext() != NULL) {
-pop(head->getNext());
+if(current->getNext()->getNext() != NULL) {
+pop(head, current->getNext());
 
 } else {
-node* temp = new node(head->getNext()->getValue());
-head->getNext()->setNext(NULL);
-return temp;
+
+current->getNext()->setNext(NULL);
+return current->getNext();
 }
 
 }
@@ -90,13 +114,24 @@ return NULL;
 
 
 node* getTail(node* head) {
+  
+   
     if(head->getNext() != NULL) {
+     
+        cout << "1" << endl;
+        
         getTail(head->getNext());
         
     } else {
+       
+        cout << "2" << endl;
+        
+        
         return head;
         
     }
+    
+  
     return NULL;
     
 }
@@ -123,6 +158,8 @@ bool associative(char c) {
 
 
 int prec(char first) {
+    
+
     if(first == '^') {
         return 3;
         
@@ -144,87 +181,109 @@ void shunt(char array[80], node* &stackHead, node* &queueHead) {
 int i = 0;
 for(i= 0; array[i] != '\0'; i++) {
 if(isdigit(array[i]) == true) {
-char* data = new char[50];
-data = &array[i];
+ 
+    cout << "a" << endl;
+    char* data = new char(array[i]);
 node* temp = new node(data);
-   
+
 enqueue(queueHead,queueHead ,temp);
-    cout << queueHead->getValue();
-
-   
-
-
-// ||
-   //
-} else if(array[i] == '+' || '-' || '^' || '/' || '*') {
-    return;
     
-    while((prec(*(stackHead->getValue())) >  prec(array[i])) || (prec(*(stackHead->getValue())) == prec(array[i] && associative(array[i])))) {
-        pop(stackHead);
-        enqueue(queueHead,queueHead,stackHead);
-        
+    
+   
+ 
+
+
+
+} else if(array[i] == '+' || array[i] =='-' ||array[i] == '^' ||array[i] == '/' || array[i] == '*') {
+    
+    cout << "b" << endl;
+    if(!isEmpty(stackHead)) {
+    while((prec(*(stackHead->getValue())) > prec(array[i]) || (prec(*(stackHead->getValue())) == prec(array[i]) && associative(array[i]))) && *(stackHead->getValue()) != '(' && (!isEmpty(stackHead))) {
+        cout << "c" << endl;
+      
+         enqueue(queueHead,queueHead,stackHead);
+        pop(stackHead,stackHead);
+        cout << "d" << endl;
+       
+      
         
     }
         
-        
-        
     
-        
-        
-        
-        
+    }
     
-
-
-
+        cout << "l" << endl;
+    char* data = new char(array[i]);
+    node* temp = new  node(data);
+    cout << "y" << endl;
+ 
+    push(stackHead,stackHead, temp);
+    
+    
 } else if(array[i] == '(' ) {
-char* data = new char[50];
-data = &array[i];
+  
+char* data = new char(array[i]);
+
 node* temp = new node(data);
-push(stackHead, temp);
-
-
-} else if(array[i] == ')') {
-
-    char* leftParen = new char[50];
-    leftParen[1] = '(';
-while(*(getTail(stackHead) -> getValue()) != '(') {
-pop(stackHead);
-enqueue(queueHead,queueHead,stackHead);
-
-}
-if(*(getTail(stackHead)->getValue()) == ')') {
-pop(stackHead);
-
-}
-
-
-
-
-}
-
-
-
-if(array[i] == '\0') {
-while(stackHead != NULL) {
-pop(stackHead);
-enqueue(queueHead,queueHead,stackHead);
-
-
-
-}
-
-}
-
-
-
-
-}
-    
+push(stackHead,stackHead ,temp);
+    cout << "e" << endl;
     
   
     
    
+    
+   
+ 
+
+
+} else if(array[i] == ')') {
+    
+    cout << "f" << endl;
+   
+while(*(getTail(stackHead) -> getValue()) != '(') {
+  
+pop(stackHead,stackHead);
+enqueue(queueHead,queueHead,stackHead);
+
+    cout << "h" << endl;
+
+}
+    
+    cout << "q" << endl;
+if(*(getTail(stackHead)->getValue()) == '(') {
+    cout << "i" << endl;
+pop(stackHead,stackHead);
+    
+    cout << "j" << endl;
+
+}
+  cout << "g" << endl;
+}
+
+}
+    
+
+        
+        while(stackHead != NULL) {
+            cout << "z" << endl;
+            enqueue(queueHead,queueHead,stackHead);
+            pop(stackHead,stackHead);
+            
+            
+        }
+        
+    
+    
+    
+  
+
+    
+    while(queueHead!= NULL) {
+        cout << queueHead->getValue();
+        queueHead = queueHead->getNext();
+        
+    }
+    cout << endl;
     return;
 
 
